25887ca6c74b7e53bdd08aa0b54d1457e7aaac3e
PhuNguyen8903 <ngngocphu8903@gmail.com>
2026-05-19 01:17:03 +0700

    finish html+css login page

--- js/mock-data.js
+++ js/mock-data.js
@@ -1,10 +1,23 @@
 const Users = [
     { 
+        userID: 1,
         Username: "Admin",
         password: "Admin@123",
         profileImg: "",
         fullName: "Phune",
         gender: "Male",
         phoneNumber: "123456789123",
+        Role:"Admin"
     },
+    {
+        userID: 2,
+        Username: "nhanvien1",
+        password: "nhanvien@123",
+        profileImg: "",
+        fullName: "nv1",
+        gender: "Male",
+        phoneNumber: "123456789123",
+        Role:"Nv"
+    }
 ]
+
